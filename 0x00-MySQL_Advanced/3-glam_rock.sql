21e949680133642369759f31f79748c92c319ee6
MissTipo <dorine.a.tipo@gmail.com>
2023-04-08 22:58:49 +0300

    change orderby

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,11 @@
--- lists all bands with Glam rock as their main style
--- ranked by their longevity
-SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan 
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, ((CASE WHEN split IS NULL THEN 2022 ELSE split END) - formed) AS lifespan 
 FROM metal_bands 
 WHERE style LIKE '%Glam rock%'
 ORDER BY
-CASE WHEN (COALESCE(split, 2022) - formed) != 0
-  THEN lifespan
-  ELSE 0
+CASE WHEN ((CASE WHEN split IS NULL THEN 2022 ELSE split END) - formed) != 0
+	THEN lifespan
+	ELSE 0
 END DESC,
 band_name DESC;
 
