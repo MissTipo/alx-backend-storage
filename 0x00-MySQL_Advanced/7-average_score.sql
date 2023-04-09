ff4b5caa35d278eeb90f864f02d7eb54d9395348
MissTipo <dorine.a.tipo@gmail.com>
2023-04-09 05:02:16 +0300

    compute avarage sc ore

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,9 @@
+-- reates a stored procedure that computes and store the average score for a student
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+  DECLARE avg_score FLOAT;
+  SET avg_score = (SELECT AVG(score) FROM corrections AS c WHERE c.user_id = user_id);
+UPDATE users SET avarage_score = avg_score WHERE id = user_id;
+END
+//
