225f1928b09850579a154b99382e29f3e0df023b
MissTipo <dorine.a.tipo@gmail.com>
2023-04-09 04:41:44 +0300

    create stored precedure

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,15 @@
 -- Creates a stored procedure that adds a new correction for a student
-delimiter //
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
+DELIMITER //
+CREATE PROCEDURE AddBonus (
+  IN user_id int,
+  IN project_name varchar(255),
+  IN score float)
 BEGIN
-  INSERT INTO projects(name)
+  INSERT INTO projects (name)
   SELECT project_name FROM DUAL
-  WHERE project_name NOT IN (SELECT name FROM projects);
+
+  WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
   INSERT INTO corrections (user_id, project_id, score)
-  VALUES (user_id, (SELECT id FROM projectS WHERE name = projects_name), score);
+  VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END;
 //
