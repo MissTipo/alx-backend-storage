dc21183676edf1188a3dd24e4fbdd2c60419a333
MissTipo <dorine.a.tipo@gmail.com>
2023-04-12 05:33:57 +0300

    import the pymongo module

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 '''python function that lists all documents in a collection'''
+import pymongo
+
 
 def list_all(mongo_collection):
+    '''lists all documents in a collection'''
     cursor = mongo_collection.find()
     result = []
     for document in cursor:
